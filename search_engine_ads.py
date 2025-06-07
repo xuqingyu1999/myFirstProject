d392e17caa11a435e8bd307a3e580553e16bfe41
xuqingyu1999 <142370351+xuqingyu1999@users.noreply.github.com>
2025-06-07 09:47:27 +0800

    Update search_engine_ads.py

--- search_engine_ads.py
+++ search_engine_ads.py
@@ -44,7 +44,7 @@ def save_to_gsheet(data):
         ]
     )
     client = gspread.authorize(creds)
-    sheet = client.open("Click History").sheet4
+    sheet = client.open("SeEn Ads").sheet4
     sheet.append_row([data[k] for k in ["id", "timestamp", "type", "title", "url"]])
 ############################################
 # Step 0: Page config & DeepSeek client
